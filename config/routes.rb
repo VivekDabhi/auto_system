110ea0f426439ff6b9b8c56139cf90b9e487e07d
VivekDabhi <vivekdabhi80@gmail.com>
2020-05-12 17:53:45 +0900

    add devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # devise_for :admin_users
+  # root to: "home#index"
+
+  devise_for :admin_users, controllers: {
+    sessions: 'admin_users/sessions'
+  }
 end
